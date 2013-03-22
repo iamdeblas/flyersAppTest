b04373dd6cc9cc8c9b380a22abc85fbb06a95361
Anthony Bruno <abdeblasio@gmail.com>
2013-03-22 10:27:28 -0400

    v 0.5

---
+++ jqueryMob.js
@@ -0,0 +1,33 @@
+
+$(document).ready (function(){
+	
+$('#roster').click(function(){
+	window.location.href = "http://flyers.nhl.com/club/roster.htm";
+	});
+
+$('#schedule').click(function(){
+	window.location.href = "http://flyers.nhl.com/club/schedule.htm";
+	});
+
+$('#record').click(function(){
+	window.location.href = "http://flyers.nhl.com/club/gamelog.htm";
+	});
+
+$('#scratches').click(function(){
+	window.location.href = "http://flyers.nhl.com/club/page.htm?id=36232";
+	});
+
+$('#updates').click(function(){
+	window.location.href = "http://flyers.nhl.com/club/page.htm?id=64095";
+	});
+
+$('#botsocfb').click(function(){
+	window.location.href = "http://www.facebook.com/philadelphiaflyers?fref=ts";
+	});
+
+$('#botsoctw').click(function(){
+	window.location.href = "http://twitter.com/NHLFlyers";
+	});
+
+});
+
