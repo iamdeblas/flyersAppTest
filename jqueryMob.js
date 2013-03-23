a023b9112a0a36961e86d51edb353c2b0ea6c430
Anthony Bruno <abdeblasio@gmail.com>
2013-03-23 16:26:07 -0400

    attrAddition

--- jqueryMob.js
+++ jqueryMob.js
@@ -1,32 +1,39 @@
 
 $(document).ready (function(){
-	
+
 $('#roster').click(function(){
-	window.location.href = "http://flyers.nhl.com/club/roster.htm";
+	document.location.href = "http://flyers.nhl.com/club/roster.htm";
+$('#roster').attr("href", "http://flyers.nhl.com/club/roster.htm");
 	});
 
 $('#schedule').click(function(){
-	window.location.href = "http://flyers.nhl.com/club/schedule.htm";
+	document.location.href = "http://flyers.nhl.com/club/schedule.htm";
+$('#schedule').attr("href", "http://flyers.nhl.com/club/schedule.htm");
 	});
 
 $('#record').click(function(){
-	window.location.href = "http://flyers.nhl.com/club/gamelog.htm";
+	document.location.href = "http://flyers.nhl.com/club/gamelog.htm";
+$('#record').attr("href", "http://flyers.nhl.com/club/gamelog.htm");
 	});
 
 $('#scratches').click(function(){
-	window.location.href = "http://flyers.nhl.com/club/page.htm?id=36232";
+	document.location.href = "http://flyers.nhl.com/club/page.htm?id=36232";
+$('#scratches').attr("href", "http://flyers.nhl.com/club/page.htm?id=36232");
 	});
 
 $('#updates').click(function(){
-	window.location.href = "http://flyers.nhl.com/club/page.htm?id=64095";
+	document.location.href = "http://flyers.nhl.com/club/page.htm?id=64095";
+$('#updates').attr("href", "http://flyers.nhl.com/club/page.htm?id=64095");
 	});
 
 $('#botsocfb').click(function(){
-	window.location.href = "http://www.facebook.com/philadelphiaflyers?fref=ts";
+	document.location.href = "http://www.facebook.com/philadelphiaflyers?fref=ts";
+$('#botsocfb').attr("href", "http://www.facebook.com/philadelphiaflyers?fref=ts");
 	});
 
 $('#botsoctw').click(function(){
-	window.location.href = "http://twitter.com/NHLFlyers";
+	document.location.href = "http://twitter.com/NHLFlyers";
+$('#botsoctw').attr("href", "http://twitter.com/NHLFlyers");
 	});
 
 });
