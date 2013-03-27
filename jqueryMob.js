6102445b506ade3df3b668b446c7b59bc29defdf
Anthony Bruno <abdeblasio@gmail.com>
2013-03-27 11:12:54 -0400

    v1.0

--- jqueryMob.js
+++ jqueryMob.js
@@ -2,31 +2,31 @@
 $(document).ready (function(){
 
 $('#roster').click(function(){
-window.open("http://flyers.nhl.com/club/roster.htm");
+document.location.href = ("http://flyers.nhl.com/club/roster.htm");
 	});
 
 $('#schedule').click(function(){
-window.open("http://flyers.nhl.com/club/schedule.htm");
+document.location.href = ("http://flyers.nhl.com/club/schedule.htm");
 	});
 
 $('#record').click(function(){
-window.open("http://flyers.nhl.com/club/gamelog.htm");
+document.location.href = ("http://flyers.nhl.com/club/gamelog.htm");
 	});
 
 $('#scratches').click(function(){
-window.open("http://flyers.nhl.com/club/page.htm?id=36232");
+document.location.href = ("http://flyers.nhl.com/club/page.htm?id=36232");
 	});
 
 $('#updates').click(function(){
-window.open("http://flyers.nhl.com/club/page.htm?id=64095");
+document.location.href = ("http://flyers.nhl.com/club/page.htm?id=64095");
 	});
 
 $('#botsocfb').click(function(){
-window.open("http://www.facebook.com/philadelphiaflyers?fref=ts");
+document.location.href = ("http://www.facebook.com/philadelphiaflyers?fref=ts");
 	});
 
 $('#botsoctw').click(function(){
-window.open("http://twitter.com/NHLFlyers");
+document.location.href = ("http://twitter.com/NHLFlyers");
 	});
 });
 
